ff3c61c6fcdbc27d13bac458519310e5acbdba7c
Manuel Schmid <manysch3@gmail.com>
2021-10-13 23:54:03 +0200

    implemented dynamic adding of new entries for the current date

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
     auto tm = *std::localtime(&t);
     std::ostringstream oss;
     oss << std::put_time(&tm, "%d.%m.%Y");
-    auto date = oss.str();
+    auto today = oss.str();
 //    std::cout << date << std::endl;
 
     // read a JSON file
@@ -27,12 +27,21 @@ int main() {
 
     // read data
     double tdyValue;
-    for (auto& el : j[date].items())
-    {
-        tdyValue = el.value();
+    try {
+        if (j[today] == nullptr) {
+            throw 404;
+        } else {
+            for (auto& el : j[today].items())
+            {
+                tdyValue = el.value(); // read combined total of today's entries
+            }
+        }
+    }
+    catch (...) {
+        tdyValue = 0.00; // if there was no entry today set to 0.00
     }
 
-    j["13.10.2021"] = tdyValue + 3.00;
+    j[today] = tdyValue + 3.00;
 
     // write prettified JSON to another file
     std::ofstream o("pretty.json");
