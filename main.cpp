28c30fd21cda672437e67b1f2b8bb15353b47ee9
Manuel Schmid <manysch3@gmail.com>
2021-10-13 21:43:22 +0200

    added basic csv creation structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Create an output filestream object
+    std::ofstream myFile("foo.csv");
+
+    // Send data to the stream
+    myFile << "Foo\n";
+    myFile << "1\n";
+    myFile << "2\n";
+    myFile << "3\n";
+
+    // Close the file
+    myFile.close();
+
     return 0;
 }
