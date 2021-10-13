2566958d3dcf98dc153cc5ed0517b7d2becbf110
Manuel Schmid <manysch3@gmail.com>
2021-10-13 21:54:55 +0200

    added write_csv function to write integers into csv file

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <fstream>
+#include <vector>
 
-int main() {
+void write_csv(std::string filename, std::string colname, std::vector<int> vals){
+    // Make a CSV file with one column of integer values
+    // filename - the name of the file
+    // colname - the name of the one and only column
+    // vals - an integer vector of values
+
     // Create an output filestream object
-    std::ofstream myFile("foo.csv");
+    std::ofstream myFile(filename);
+
+    // Send the column name to the stream
+    myFile << colname << "\n";
 
     // Send data to the stream
-    myFile << "Foo\n";
-    myFile << "1\n";
-    myFile << "2\n";
-    myFile << "3\n";
+    for(int i = 0; i < vals.size(); ++i)
+    {
+        myFile << vals.at(i) << "\n";
+    }
 
     // Close the file
     myFile.close();
+}
+
+int main() {
+    // Make a vector of length 100 filled with 1s
+    std::vector<int> vec(100, 1);
+
+    // Write the vector to CSV
+    write_csv("ones.csv", "Col1", vec);
 
     return 0;
 }
+
